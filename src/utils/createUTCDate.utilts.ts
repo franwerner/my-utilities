44a75234c20e4927372c60f49883ad7556689518
Franco Werner <ifrank44445@gmail.com>
2025-02-03 05:19:29 -0300

    c

--- src/utils/createUTCDate.utilts.ts
+++ src/utils/createUTCDate.utilts.ts
@@ -1,6 +1,7 @@
-const createUTCDate = ({ hours = 0, minutes = 0, seconds = 0 }) => {
+const createUTCDate = ({ hours = 0, minutes = 0, seconds = 0,days = 0 }) => {
     const date = new Date()
     hours && date.setUTCHours(date.getUTCHours() + hours)
+    days && date.setUTCDate(date.getUTCDate() + days)
     minutes && date.setUTCMinutes(date.getUTCMinutes() + minutes)
     seconds && date.setUTCSeconds(date.getUTCSeconds() + seconds)
     return date
